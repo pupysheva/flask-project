505ada392c3d097cd413260002c173e34dbf8b1c
SG_Muwa <motherlode.muwa@gmail.com>
2020-04-02 01:59:05 +0300

    Debug two apps running

--- my_module_interface.py
+++ my_module_interface.py
@@ -1,25 +1,52 @@
-from .my_module import RecommendationAlgoritm
+from my_module import RecommendationAlgoritm
 import struct
 
-def get_recommendation(user_id):
-    ra = RecommendationAlgoritm()
-    return ra.get_recommendation(user_id)
+def lowpriority():
+    """ Set the priority of the process to below-normal."""
+
+    import sys
+    try:
+        sys.getwindowsversion()
+    except AttributeError:
+        isWindows = False
+    else:
+        isWindows = True
+
+    if isWindows:
+        # Based on:
+        #   "Recipe 496767: Set Process Priority In Windows" on ActiveState
+        #   http://code.activestate.com/recipes/496767/
+        import win32api,win32process,win32con
+
+        pid = win32api.GetCurrentProcessId()
+        handle = win32api.OpenProcess(win32con.PROCESS_ALL_ACCESS, True, pid)
+        win32process.SetPriorityClass(handle, win32process.IDLE_PRIORITY_CLASS)
+    else:
+        import os
+
+        os.nice(19)
 
 class ProgressInFile:
     def __init__(self, id_thread):
         self._progress = 0
-        self.f = open('thread_' + id_thread, 'rb')
+        self.f = open('thread_' + str(id_thread), 'wb')
     
     def set_progress(self, p: float):
         self.f.seek(0)
-        self.f.write(struct.pack(f, float(p)))
+        self.f.write(struct.pack('f', float(p)))
         self.f.flush()
         self._progress = p
-    
-    def __del__(self):
-        f.close();
+        
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.f.close();
 
 def train_model(id_thread):
+    lowpriority()
     ra = RecommendationAlgoritm()
-    with ProgressInFile() as f:
+    with ProgressInFile(id_thread) as f:
         ra.train_model(f)
+    return ra
+    
--- start.py
+++ start.py
@@ -1,13 +1,16 @@
 from my_module import RecommendationAlgoritm
+import my_module_interface
 
 from flask import Flask, render_template, current_app
 from multiprocessing import Process
+import subprocess
 
 
 import random
+import struct
 import threading
 import time
-import psutil
+import os
 
 
 app = Flask(__name__, static_url_path='',
@@ -28,48 +31,22 @@ def get_recommendation(user_id):
 
 @app.route('/train', methods=["POST"])
 def train_model():
-    global rec_alg
-    return get_start_thread(lambda progress: rec_alg.train_model(progress))
-
-
-class ExportingThread(threading.Thread):
-    def __init__(self):
-        self.progress = 0
-        super().__init__()
-
-    def set_callback_thread(self, callback_thread):
-        self.callback_thread = callback_thread
-    
-    def set_progress(self, p: float):
-        print("%s%%" % (p * 100))
-        self.progress = p
-
-    def run(self):
-        try:
-            self.callback_thread(self)
-        finally:
-            self.progress = 1
-
-
-exporting_threads = {}
-
-
-def get_start_thread(callback_thread):
-    global exporting_threads
-
-    thread_id = random.randint(0, 10000)
-    exporting_threads[thread_id] = ExportingThread()
-    exporting_threads[thread_id].set_callback_thread(callback_thread)
-    exporting_threads[thread_id].start()
-
+    thread_id = random.randint(0, 100000)
+    p = Process(target=my_module_interface.train_model, args=(thread_id,))
+    p.start()
     return str(thread_id)
-
 
 @app.route('/progress/<int:thread_id>')
 def progress(thread_id):
-    global exporting_threads
-
-    return str(exporting_threads[thread_id].progress)
+    filename = 'thread_' + str(thread_id)
+    if os.path.exists(filename):
+        with open(filename, 'rb+') as f:
+            data = str(struct.unpack('f', f.read()))
+        os.reamove(filename)
+    else:
+        data = 0
+    return str(data)
+    
 
 
 if __name__ == '__main__':
