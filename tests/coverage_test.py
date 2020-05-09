7fec23992aa6352be4d1216da33b8ae8fa497065
SG_Muwa <motherlode.muwa@gmail.com>
2020-05-09 19:05:29 +0300

    Add support threads in coverage_test.py for Microsoft Windows

--- tests/coverage_test.py
+++ tests/coverage_test.py
@@ -6,37 +6,25 @@ import time
 import sys
 sys.path.append('./')
 from reco_engine import RecommendationAlgorithm
-from multiprocessing import Pool
 import os
-import threading
 from datetime import datetime
+from multiprocessing import Process, Queue
 
 def init():
     global g_rec_alg
     global g_user_ids_list
-    global g_items_in_rec
-    global g_user_with_rec
-    global g_locks
     g_rec_alg = RecommendationAlgorithm(from_pkl=True)
     # Получить список всех пользователей
     g_user_ids_list = g_rec_alg.data_with_user["u_id"].unique()
     print(len(g_user_ids_list))
-    g_items_in_rec = {}
-    g_user_with_rec = []
-    g_locks = [threading.Lock(), threading.Lock()]
 
-def pred_thread(id_thread):
-    global g_users
-    global g_user_with_rec
-    global g_items_in_rec
-    global g_locks
-    users = g_users
+def pred_thread(rec_alg, users, queue, id_thread):
     user_with_rec = []
     items_in_rec = {}
     now = time.time()
     for ep, user in enumerate(users):
         if ep % os.cpu_count() == id_thread:
-            recset = g_rec_alg.get_recommendation(user, if_need_print_time=False)
+            recset = rec_alg.get_recommendation(user, if_need_print_time=False)
             if not recset.empty:
                 user_with_rec.append(user)
                 for rec in recset["i_id"].values:
@@ -47,38 +35,24 @@ def pred_thread(id_thread):
             if ep % 100 == 99:
                 print(datetime.now(), (time.time() - now) / 100)
                 now = time.time()
-    print(datetime.now(), 'start merge by thread', id_thread)
-    lock_ok = [False, False]
-    while not (lock_ok[0] and lock_ok[1]):
-        if not lock_ok[0]:
-            lock_ok[0] = g_locks[0].acquire(False)
-            if lock_ok[0]:
-                print(datetime.now(), 'merge results to g_user_with_rec by thread', id_thread)
-                g_user_with_rec.extend(user_with_rec)
-                g_locks[0].release()
-        if not lock_ok[1]:
-            lock_ok[1] = g_locks[1].acquire(False)
-            if lock_ok[1]:
-                print(datetime.now(), 'merge results to g_items_in_rec by thread', id_thread)
-                for key, value in items_in_rec.items():
-                    if key in g_items_in_rec:
-                        g_items_in_rec[key] += value
-                    else:
-                        g_items_in_rec[key] = value
-                g_locks[1].release()
-        if not (lock_ok[0] and lock_ok[1]):
-            time.sleep(0.01)
-    print(datetime.now(), 'finish thread', id_thread)
+    print(datetime.now(), 'finish tread', id_thread)
+    queue.put((user_with_rec, items_in_rec))
 
 def calculate_coverage(users):
-    global g_users
-    global g_user_with_rec
-    global g_items_in_rec
-    global g_user_ids_list
-    g_users = users
-    with Pool(os.cpu_count()) as p:
-        p.map(pred_thread, range(os.cpu_count()))
-
+    g_items_in_rec = {}
+    g_user_with_rec = []
+    q = Queue()
+    for i in range(os.cpu_count()):
+        p = Process(target=pred_thread, args=(g_rec_alg, users, q, i))
+        p.start()
+    for i in range(os.cpu_count()):
+        (user_with_rec, items_in_rec) = q.get()
+        g_user_with_rec.extend(user_with_rec)
+        for key, value in items_in_rec.items():
+            if key in g_items_in_rec:
+                g_items_in_rec[key] += value
+            else:
+                g_items_in_rec[key] = value
     print("g_items_in_rec", g_items_in_rec, len(g_items_in_rec.items()))
     no_movies = 27278
     no_movies_in_rec = len(g_items_in_rec.items())
