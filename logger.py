af7c291940636d7d8751c3ebd746cc0f5a8ef8d4
SG_Muwa <motherlode.muwa@gmail.com>
2020-06-06 19:04:09 +0300

    logger: Add dict, function, list

--- logger.py
+++ logger.py
@@ -4,14 +4,38 @@ from psutil import cpu_percent, virtual_memory, swap_memory
 from os import cpu_count
 from datetime import datetime
 from platform import system, release
+from collections.abc import Iterable
 
 oldprint = datetime.now()
 
 def startup():
-    log('startup: total VIRT: {:>6.0f} MiB; total SWAP: {:>7.0f} MiB; threads: {}; system: {} {}'.format(virtual_memory().total / 2**20, swap_memory().total / 2**20, cpu_count(), system(), release()))
-def log(message = ""):
+    log({'total VIRT': '{:.0f} MiB'.format(virtual_memory().total / 2**20),
+         'total SWAP': '{:.0f} MiB'.format(swap_memory().total / 2**20),
+         'threads': cpu_count(),
+         'system': '{} {}'.format(system(), release())
+    }, startup)
+def log(message = "", method = None):
+    if isinstance(message, str):
+        message = message.split('\n')
+        loglines(message, method)
+    elif isinstance(message, dict):
+        for pair in message.items():
+            logline('{}: {}'.format(pair[0], pair[1]), method)
+    elif isinstance(message, Iterable):
+        for m in message:
+            log(m, method)
+    else:
+        logline(str(message), method)
+def logline(message = "", method = None):
     global oldprint
     newprint = datetime.now()
-    print('{} [+{}] VIRT: {:>6.0f} MiB; SWAP: {:>7.0f} MiB; CPU: {:>5.1f} %: {}'.format(datetime.now(), newprint - oldprint, virtual_memory().used / 2**20, swap_memory().used / 2**20, cpu_percent(), message))
+    print('{} [+{}] VIRT: {:>6.0f} MiB; SWAP: {:>7.0f} MiB; CPU: {:>5.1f} %{}: {}'.format(datetime.now(), newprint - oldprint, virtual_memory().used / 2**20, swap_memory().used / 2**20, cpu_percent(), '; {}'.format(method.__name__) if method is not None else "", message))
     oldprint = datetime.now()
+def loglines(messages = [], method = None):
+    f = True
+    for m in filter(lambda s: s is not None and s != "", messages):
+        f = False
+        logline(m, method)
+    if f:
+        logline("", method)
 startup()
